dcffa0b57baa1b9c4ad1c1243382d22cbada3ffb
Kazim <mkaxymkhan3@gmail.com>
2022-04-29 22:21:47 +0500

    first commit

---
+++ Guess game/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string SecretWord = "pakistan";
+            string guess = "";
+            int guesscount = 0;
+            int guesslimit = 3;
+            bool outofguesses = false;
+
+            while (guess != SecretWord && !outofguesses)
+            {
+                if (guesscount < guesslimit)
+                {
+                    Console.WriteLine("Enter Guess: ");
+                    guess = Console.ReadLine();
+                    guesscount++;
+                }
+                else
+                {
+                    outofguesses = true;
+
+
+                }
+            }
+                if (outofguesses)
+                {
+                    Console.WriteLine("You Loose");
+                }
+                else
+                {
+                    Console.WriteLine("You Win!");
+                }
+                    Console.ReadLine();
+
+            
+           
+        }
+    }
+}
